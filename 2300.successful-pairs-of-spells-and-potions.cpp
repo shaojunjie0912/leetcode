0d234cf551967513429025076051cf9866959d14
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-03-08 09:50:39 +0800

    update

--- 2300.successful-pairs-of-spells-and-potions.cpp
+++ 2300.successful-pairs-of-spells-and-potions.cpp
@@ -10,7 +10,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        
+        // vector<int> pairs; // NOTE: 后面直接修改 spells, 省空间
+        ranges::sort(potions);  // 对 potions 进行从小到大排序
+
+        // 遍历 spells, 在 potions 中找到 >= success/spells[i] 的第一个数
+        for (auto& sp : spells) {
+            // NOTE: success 是 int64_t, target 也要!
+            // NOTE: a/b向上取整: (a+b-1)/b
+            int64_t target = (success + sp - 1) / sp;
+
+            // HACK: 由于 potions[int] 最大值确定, 因此提前判断
+            // 思考: potions 中元素都是 int, 不加判断则都被转为 int64_t 浪费
+            // 加了判断只要将 target 转 int, 因为 potions 一定都在 int 范围内
+            if (target <= potions.back()) {
+                // NOTE: 用 end 减 iter, 一开始写反了
+                // 原地修改
+                sp = std::end(potions) - ranges::lower_bound(potions, (int)target);
+            } else {
+                sp = 0;
+            }
+        }
+        return spells;
     }
 };
 // @leet end
---
+++ 2563.count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#include "include/list_node.hpp"
+#include "include/node.hpp"
+#include "include/tree_node.hpp"
+
+using namespace std;
+
+// @leet start
+class Solution {
+public:
+    // 排序后二分
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        ranges::sort(nums);
+        int64_t ans{0};
+        // 两个边界
+        // lower <= nums[i] + nums[j] <= upper
+        // lower - nums[i] <= nums[j] <= upper - nums[i]
+        for (auto it{begin(nums)}; it != end(nums) - 1; ++it) {
+            // HACK: 傻孩子, lower/upper 不能累加被更改啊啊啊啊! 重新定义变量
+            int new_lower = lower - *it;
+            int new_upper = upper - *it;
+
+            // NOTE: 傻孩子, 你只在 for 循环条件中考虑到了要从 it 后面找
+            // 这里也要修改范围呀!
+            // 第一个 >= lower
+            auto first_greater_lower{ranges::lower_bound(it + 1, end(nums), new_lower)};
+            // 最后一个 <= upper
+            auto last_less_upper{ranges::lower_bound(it + 1, end(nums), new_upper + 1) - 1};
+            ans += last_less_upper - first_greater_lower + 1;
+        }
+        return ans;
+    }
+};
+// @leet end
+
+int main() {
+    return 0;
+}

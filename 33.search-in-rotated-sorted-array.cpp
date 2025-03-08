9ec3139c33cb22efb76191c3adcc75bec5991003
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-03-08 21:36:46 +0800

    update

--- 33.search-in-rotated-sorted-array.cpp
+++ 33.search-in-rotated-sorted-array.cpp
@@ -6,11 +6,38 @@
 
 using namespace std;
 
+// 二分 NOTE: 这里不能简单的直接用nums[mid]</>=target分区
+// 要考虑数组被划分为了两个递增段
+
+// [第一段 ↑, 第二段 ↑]
+// 在第一段还是第二段可以通过跟最右侧 nums[n-1] 比较
+
+// 红色: nums[mid] 在 target 左侧(不是小于的意思)
+
+// 蓝色: nums[mid] 就是 target 或在 target 右侧(不是大于的意思)
+// - nums[mid] 在第一段, target 也在第一段, 且 target<=nums[mid]
+// - nums[mid] 在第二段
+//                      target 在第一段且 target <=nums[mid]
+//                      target 在第二段且 target<=nums[mid]
+
+// 答案总在红蓝边界?(蓝区第一个元素)
+
 // @leet start
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+        int l = 0;
+        int r = nums.size() - 2;  // 最后一个元素一定是蓝色!
+        // 而且 l 最终可能要来到最后一个位置
+        while (l <= r) {  // HACK: 我习惯闭区间[0, n-2]了, 开区间是(-1,n-1)
+            int mid = l + (r - l) / 2;
+            if () {  // 蓝色, 往左侧红色移动
+                r = mid - 1;
+            } else {  // 红色, 往右侧蓝色移动
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 };
 // @leet end
--- 704.binary-search.cpp
+++ 704.binary-search.cpp
@@ -5,23 +5,42 @@
 
 using namespace std;
 
+// 二分红蓝染色法
+
+// LowerBound(其实C++20有ranges::lower_bound>=, 以及upper_bound>)
+// 红: nums[mid] < target
+// 蓝: nums[mid] >= target
+
+// 对于 [0, n-1] 二分, 最终 l 到 n
+// 意味着最后返回 l 要<额外>再做一个判断是否等于 n, 代表是否搜索到
+// 搜索到了再判断 nums[l] 是否是 target 即可
+
+// NOTE: 灵神说区间改为 [0, n-2], 这样最终 l 来到 n-1,
+// 只要判断 nums[l] 是否是 target 即可
+
 // @leet start
 class Solution {
 public:
-    int search(vector<int> &nums, int target) {
+    int search(vector<int>& nums, int target) {
+        int l = LowerBound(nums, target);
+        return (l < nums.size() && nums[l] == target) ? l : -1;  // [0, n-1] 要判断 2 个条件
+        // return (nums[l] == target) ? l : -1;                     // [0, n-2] 只要 1 个条件
+    }
+
+private:
+    int LowerBound(vector<int>& nums, int target) {
         int l = 0;
-        int r = nums.size() - 1;
+        int r = nums.size() - 1;  // [0, n-1]
+        // int r = nums.size() - 2; // [0, n-2]
         while (l <= r) {
-            int mid = l + (r - l) / 2;  // NOTE: 这在里面!!!傻了
-            if (nums[mid] < target) {
-                l = mid + 1;
-            } else if (nums[mid] > target) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {  // 当前及右边是蓝色, 需要往左
                 r = mid - 1;
-            } else {
-                return mid;
+            } else {  // 当前及左边是红色, 需要往右
+                l = mid + 1;
             }
         }
-        return -1;
+        return l;
     }
 };
 // @leet end

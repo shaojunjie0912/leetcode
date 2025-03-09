729e52128d7c8cb5388faf3fa75cba92e22c0273
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-03-09 11:27:28 +0800

    update

--- 1343.number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343.number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 // 定长滑动窗口
-// 这道题同理, 当第一个窗口还没形成, 就无法比较
+// 这道题一样可以将均值阈值转化为和阈值, 减少循环内计算均值
 
 // @leet start
 class Solution {
@@ -15,15 +15,18 @@ public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
         int ans{0};
         int curr_sum{0};
+        int sum_thresh{threshold * k};         // mean阈值转化为sum阈值
         for (int i{0}; i < arr.size(); ++i) {  // 入
             curr_sum += arr[i];
-            curr_mean = curr_sum / k;
 
             if (i < k - 1) {
                 continue;
             }
 
             // 更新
+            if (curr_sum >= sum_thresh) {
+                ++ans;
+            }
 
             // 出
             curr_sum -= arr[i - k + 1];

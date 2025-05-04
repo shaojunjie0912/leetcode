ad286e541cd3e8ff590ee5c3cecab45288a30c78
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-05-04 19:04:38 +0800

    update

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,6 +6,22 @@
 
 using namespace std;
 
+// 思考递归:
+
+// 原问题:
+// 前序遍历 preorder[0] 是根
+//           ↓
+// 中序遍历 inorder 中根左边: 左子树及其大小, 根右边: 右子树及其大小
+//           ↓
+// 前序遍历 preorder 左子树, 右子树
+//           ↓
+// 子问题:
+// 左右子树的前序遍历和中序遍历
+
+// 优化:
+// 1. 哈希表记录 inorder 每个元素对应的下标, 这样就能 O(1) 找到 preorder[0]
+// 2. 递归参数改为子数组下标区间 (左闭右闭), 避免复制
+
 // @leet start
 /**
  * Definition for a binary tree node.
@@ -20,42 +36,52 @@ using namespace std;
  */
 class Solution {
 public:
-    // NOTE: 递归
-    // 根据先序+中序特点
-    // 先序[l1, ..., r1] l1 位置肯定是根节点
-    // 中序[l2, ...k..., r2] l2 ~ k-1 范围肯定是左数的节点
+#if 1
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        // HACK: 优化: 使用哈希表提前记录inorder所有节点索引, 避免find每次查找
-        unordered_map<int, int> in_map;
-        for (int i{0}; i < inorder.size(); ++i) {
-            in_map[inorder[i]] = i;
+        int n = inorder.size();
+        unordered_map<int, int> index_map;
+        for (int i = 0; i < n; ++i) {
+            index_map[inorder[i]] = i;  // 记录 inorder 元素和对应索引
         }
-        return PreInOrderBuild(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1,
-                               in_map);
+        // pre: [pre_l, pre_r)
+        // in: [in_l, in_r)
+        function<TreeNode*(int, int, int, int)> dfs = [&](int pre_l, int pre_r, int in_l,
+                                                          int in_r) -> TreeNode* {
+            // NOTE: 边界条件: 左闭右开区间, 跟 vector 区间差不多?
+            if (pre_l == pre_r) {
+                return nullptr;
+            }
+            // pre_l: 前序遍历结果数组第一个元素
+            int left_size = index_map[preorder[pre_l]] - in_l;
+            auto left = dfs(pre_l + 1, pre_l + 1 + left_size, in_l, in_l + left_size);
+            auto right = dfs(pre_l + 1 + left_size, pre_r, in_l + 1 + left_size, in_r);
+            return new TreeNode{preorder[pre_l], left, right};
+        };
+        return dfs(0, n, 0, n);
     }
-
-    // 1. 先有根节点
-    // 2. 构建左子树
-    // 3. 构建右子树
-    TreeNode* PreInOrderBuild(vector<int>& pre, int l1, int r1, vector<int>& in, int l2, int r2,
-                              unordered_map<int, int>& in_map) {
-        // HACK: 终止条件, 当[l1, r1]中 l1 == r1 时 即只有一个节点
-        if (l1 > r1) {
+#else
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.empty()) {  // NOTE: 边界条件: 空节点
             return nullptr;
         }
-        auto root{new TreeNode{pre[l1]}};
-        // 根节点在中序遍历数组的位置(根据它确定左右树范围)
-        int k = in_map[pre[l1]];
-        int left_size = k - l2;
-        // NOTE: 左右子树范围(下标换算!!)
-        root->left = PreInOrderBuild(pre, l1 + 1, l1 + left_size, in, l2, k - 1, in_map);
-        root->right =
-            PreInOrderBuild(pre, l1 + left_size + 1, r1, in, k + 1, r2, in_map);  // 左树右边界+1
-        return root;
+        // 左子树大小
+        auto left_size{ranges::find(inorder, preorder[0]) - inorder.begin()};
+
+        // 左右子树前序遍历结果数组
+        vector<int> left_preorder{preorder.begin() + 1, preorder.begin() + 1 + left_size};
+        vector<int> right_preorder{preorder.begin() + 1 + left_size, preorder.end()};
+
+        // 左右子树中序遍历结果数组
+        vector<int> left_inorder{inorder.begin(), inorder.begin() + left_size};
+        vector<int> right_inorder{inorder.begin() + left_size + 1, inorder.end()};
+
+        auto left{buildTree(left_preorder, left_inorder)};     // 递归左子树
+        auto right{buildTree(right_preorder, right_inorder)};  // 递归右子树
+
+        return new TreeNode{preorder[0], left, right};
     }
+#endif
 };
 // @leet end
 
-int main() {
-    return 0;
-}
+int main() { return 0; }

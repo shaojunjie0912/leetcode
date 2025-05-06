6b2f6999f54290bd641848ee29464c2b11ef99db
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-05-06 21:58:02 +0800

    update

--- 22.generate-parentheses.cpp
+++ 22.generate-parentheses.cpp
@@ -6,43 +6,53 @@
 
 using namespace std;
 
-// 组合/子集型回溯两种做法:
-// - 选或不选
-// - 枚举选哪个
+// 组合型回溯
 
-// 组合型回溯?
 // 左括号等价于 "选"
 // 右括号等价于 "不选"
+
 // 记左右括号总数 2 * n, 分别需要 n 个
+
 // 当前索引 i, 当前左括号数量 open, 当前右括号数量 i - open
+// 秒在: 索引从 0 开始, 那么 i 可以代表 i 个括号, 数量对齐了
+
 // 当左括号的数量 open < n, 就可以继续选左括号
 // 当右括号的数量 i - open < open, 就可以继续选右括号
 
 // @leet start
 class Solution {
 public:
+#if 1
+    // 选/不选
     vector<string> generateParenthesis(int n) {
-        int m = 2 * n;
         vector<string> ans;
-        string path(m, 0);
-        auto dfs = [&](auto&& self, int i, int open) {
-            // 边界条件
-            if (i == m) {
+        string path(2 * n, 0);  // 这是 2 * n
+        function<void(int, int)> dfs = [&](int i, int open) {
+            // 边界条件: 这是 2 * n
+            if (i == 2 * n) {
                 ans.push_back(path);
                 return;
             }
+
+            // 不选
+            if (i - open < open) {  // 还可以再选右括号
+                path[i] = ')';      // 不选也要填右括号 '('
+                dfs(i + 1, open);   // 继续递归 i+1, 左括号数量不变
+            }
+
+            // 选
             if (open < n) {  // 还可以再选左括号
                 path[i] = '(';
-                self(self, i + 1, open + 1);  // 继续递归 i+1, 左括号数量+1
-            }
-            if (i - open < open) {  // 还可以再选右括号
-                path[i] = ')';
-                self(self, i + 1, open);  // 继续递归 i+1, 左括号数量不变
+                dfs(i + 1, open + 1);  // 继续递归 i+1, 左括号数量+1
             }
         };
-        dfs(dfs, 0, 0);
+        dfs(0, 0);  // 索引 0, 也正好 0 个左括号
         return ans;
     }
+#else
+    // 懒得写枚举哪一个了
+    vector<string> generateParenthesis(int n) {}
+#endif
 };
 // @leet end
 
--- 39.combination-sum.cpp
+++ 39.combination-sum.cpp
@@ -8,28 +8,33 @@ using namespace std;
 
 // 区别于固定个数 k 的组合型回溯
 // 1. 这里没有 k 的个数限制, 只有目标和限制
-// 2. 这里的数组不一定有序, 所以从正序遍历
+// 2. 这里的数组不一定有序, 所以从正序遍历, 但其实你排完序后就可以倒序了
 
 // NOTE: 同一个数字可以无限制重复被选取
 
 // @leet start
 class Solution {
 public:
-#if 1
+#if 0
     // 输入角度: 选/不选
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> path;
         int n = candidates.size();
-        function<void(int, int)> dfs = [&](int i, int t) -> void {
-            // 这是没找到答案或 t < 0 不能再选了无奈返回的边界条件
-            if (i == n || t < 0) {
-                return;
-            }
 
+        // 优化: 从小到大排序
+        ranges::sort(candidates);
+
+        function<void(int, int)> dfs = [&](int i, int t) -> void {
             // 这其实不是边界条件, 这是找到答案了提前 return
             if (t == 0) {
                 ans.push_back(path);
+                return;
+            }
+
+            // 这是没找到答案或 t < 0 不能再选了无奈返回的边界条件
+            // 优化一下: 对于排序后的数组 t < candidates[i] 就可以退出了
+            if (i == n || t < candidates[i]) {
                 return;
             }
 
@@ -46,7 +51,37 @@ public:
         return ans;
     }
 #else
-// 答案角度: 枚举选哪个
+    // 答案角度: 枚举选哪个
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        int n = candidates.size();
+
+        ranges::sort(candidates);
+
+        // 枚举 [i, n-1]
+        function<void(int, int)> dfs = [&](int i, int t) -> void {
+            if (t == 0) {
+                ans.push_back(path);
+                return;
+            }
+
+            if (i == n || t < candidates[i]) {
+                return;
+            }
+
+            // 用 j 遍历 [i, n-1]
+            for (int j = i; j < n; ++j) {
+                path.push_back(candidates[j]);  //
+                dfs(j, t - candidates[j]);      // NOTE: 这里是 j !!!
+                path.pop_back();                // 恢复现场
+            }
+        };
+
+        dfs(0, target);
+
+        return ans;
+    }
 #endif
 };
 // @leet end

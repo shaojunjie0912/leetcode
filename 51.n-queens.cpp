852e3f50c2b72c7a665d375ae3d651d6b3ea0f93
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-05-07 09:36:49 +0800

    update

--- 51.n-queens.cpp
+++ 51.n-queens.cpp
@@ -6,6 +6,13 @@
 
 using namespace std;
 
+// 排列型回溯
+
+// 一定是每一行每一列恰好一个皇后
+// 长为 n 的数组 col: 第 i 行的皇后在第 col[i] 列
+// 则 col 数组一定是 0 ~ n - 1 的全排列
+// 本题额外多了不能同一斜线的要求
+
 // @leet start
 class Solution {
 public:

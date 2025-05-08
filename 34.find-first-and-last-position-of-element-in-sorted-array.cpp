347d4b4ff7761436360b3a03a7097c34e10ae8d7
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-05-08 13:40:29 +0800

    update

--- 34.find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // begin: [..x-3,x,x,x,x+2...] 第一个 >= x
+        // begin: [..., x-3, x, x, x, x+2, ...] 第一个 >= x
         int begin = LowerBound(nums, target);
         // 不符合条件直接返回:
         // 1. 全部都 < x, 没有 >= x 的
@@ -18,29 +18,26 @@ public:
         if (begin == nums.size() || nums[begin] != target) {
             return {-1, -1};
         }
-        // NOTE: end: [..x-3,x,x,x,x+2...] 第一个 >= x+1 的左边>那个数
+        // end: [..., x-3, x, x, x, x+2, ...] 第一个 >= x+1 的左边那个数
         int end = LowerBound(nums, target + 1) - 1;
-        // if (end == nums.size()) { // 不要判断了, 上面包括了
-        //     end = nums.size() - 1;
-        // }
         return {begin, end};
     }
 
 private:
-    // NOTE: 红蓝染色法?
+    // 红蓝染色法 (答案在红蓝边界: 蓝区域第一个)
     // 红: <
     // 蓝: >=
     // 循环结束后:
     // r: 红最后一个位置
     // l: 蓝第一个位置
 
-    // 返回<有序数组>中满足 >=target 的最小索引
-    // 如果没有, 则返回数组长度(NOTE: l 最终也就是数组长度)
+    // 返回有序数组中满足 >= target 的最小索引
+    // 如果没有, 则返回数组长度
     int LowerBound(vector<int>& nums, int target) {
-        // 闭区间
+        // 闭区间 [l, r]
         int l = 0;
         int r = nums.size() - 1;
-        // 核心就是找红蓝边界
+
         while (l <= r) {
             int mid = l + (r - l) / 2;
             // 左侧都是红色 -> 答案肯定在右侧
@@ -48,15 +45,13 @@ private:
                 l = mid + 1;           // 即此时 l-1(原来的 mid) 一定小于 target(红色)
             }
             // 右侧都是蓝色 -> 答案肯定在左侧
-            else {            // [mid] >= target 情况 NOTE: 二分搜索中 == 被剥离了
-                r = mid - 1;  // 此时 r+1(原来的 mid) 一定大于等于 target(蓝色)
+            else {            // [mid] >= target 情况
+                r = mid - 1;  // 此时 r+1 (原来的 mid) 一定 >= target (蓝色)
             }
         }
-        return l;
+        return l;  // l 即答案 (如果 = n 则说明没找到)
     }
 };
 // @leet end
 
-int main() {
-    return 0;
-}
+int main() { return 0; }
---
+++ 74.search-a-2d-matrix.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "include/list_node.hpp"
+#include "include/node.hpp"
+#include "include/tree_node.hpp"
+
+using namespace std;
+
+// 二分查找
+// 对于 m 行 n 列矩阵: 将索引 i 转化为 matrix[i / n][i % n]
+
+// @leet start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int l = 0, r = m * n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (matrix[mid / n][mid % n] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        // true 的条件:
+        // 1. l 在
+        return (l < m * n) && (matrix[l / n][l % n] == target);
+    }
+};
+// @leet end

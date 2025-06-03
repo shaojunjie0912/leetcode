95e91be22d22c1736b86e34bfad671e446735775
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-06-03 16:53:24 +0800

    update

--- 20.valid-parentheses.cpp
+++ 20.valid-parentheses.cpp
@@ -9,55 +9,31 @@ using namespace std;
 // @leet start
 class Solution {
 public:
-    // NOTE: 栈
-    // 三种 false 情况
-    // 1. 左括号多了: 当遍历完字符串时, 栈中还有元素
-    // 2. 右括号多了: 当还没遍历完字符串时, 栈已经空了
-    // 3. 左右括号不匹配: 遍历的当前括号跟栈顶(相反)对不上
-    // HACK: 技巧: <匹配左括号>时, 用<对应的右括号入栈>, 这样就可以
-    // 在遇到右括号时, 直接比较栈顶括号是否相同
-    // <遇到右括号>这是通过不满足else if [, (, { 判断的
-    // 所以不需要一个一个写右括号的判断条件
     bool isValid(string s) {
-        // HACK: 提前判断: 奇数直接false
-        if ((s.length() & 1) == 1) {
+        stack<char> st;
+        if (s.length() % 2 == 1) {  // 奇数肯定不是
             return false;
         }
-        stack<char> ch_stack;
-        for (auto& ch : s) {
-            // 遇 ( 入 )
+        for (auto ch : s) {
+            // 遇左括号则对应的右括号入栈
             if (ch == '(') {
-                ch_stack.push(')');
-            }
-            // 遇 { 入 }
-            else if (ch == '{') {
-                ch_stack.push('}');
-            }
-            // 遇 [ 入 ]
-            else if (ch == '[') {
-                ch_stack.push(']');
-            }
-            // NOTE: 检查
-            // 1. 遍历字符串期间栈空(说明右括号多了)
-            // 2. 是否匹配
-            // HACK: 栈空条件在前!!!(否则top报错)
-            else if (ch_stack.empty() || ch != ch_stack.top()) {
-                return false;
-            }
-            // 该分支说明匹配成功, 弹出栈顶括号
-            else {
-                ch_stack.pop();
+                st.push(')');
+            } else if (ch == '[') {
+                st.push(']');
+            } else if (ch == '{') {
+                st.push('}');
+            } else {
+                // 右括号
+                // 匹配失败: 1. 栈为空(没有对应左括号) 2. 左右不匹配
+                if (st.empty() || ch != st.top()) {
+                    return false;
+                }
+                st.pop();  // 匹配成功则出栈
             }
         }
-        // 遍历字符串后栈不空(说明左括号多了)
-        if (!ch_stack.empty()) {
-            return false;
-        }
-        return true;
+        return st.empty();  // NOTE: 妙: 栈空即完全匹配
     }
 };
 // @leet end
 
-int main() {
-    return 0;
-}
+int main() { return 0; }

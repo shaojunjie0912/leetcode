b7835ea15a36506097d0115acab341d16d099ce5
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-06-06 20:46:37 +0800

    update

---
+++ 349.intersection-of-two-arrays.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "include/list_node.hpp"
+#include "include/node.hpp"
+#include "include/tree_node.hpp"
+
+using namespace std;
+
+// @leet start
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_set<int> uset{nums1.begin(), nums1.end()};
+        for (auto& x : nums2) {
+            if (uset.erase(x)) {
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
+// @leet end

84e8d5581e23366f63053e675d381a2895a9487a
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-06-17 22:14:29 +0800

    update

--- 437.path-sum-iii.cpp
+++ 437.path-sum-iii.cpp
@@ -24,6 +24,7 @@
 using namespace std;
 
 // 二叉树前缀和 + 哈希表
+// NOTE: pre_sum 恢复现场 + cnt[pre_sum] 恢复现场
 
 // 二叉树前缀和: 从根节点开始的路径元素和 (从根出发, 那就前序遍历递归)
 
@@ -43,25 +44,29 @@ class Solution {
 public:
     int pathSum(TreeNode* root, int targetSum) {
         int ans{0};
-        unordered_map<int64_t, int> cnt;
-        cnt[0] = 1;  // 对应 pre_sum[0] = 0 出现一次
+        int64_t pre_sum{0};
+        unordered_map<int64_t, int> cnt;  // 前缀和出现次数
+        cnt[0] = 1;                       // 对应 pre_sum[0] = 0 出现一次
+
         // 计算二叉树前缀和
-        function<void(TreeNode*, int64_t)> dfs = [&](TreeNode* node, int64_t pre_sum) -> void {
+        function<void(TreeNode*)> dfs = [&](TreeNode* node) -> void {
             if (!node) {
                 return;
             }
+
             pre_sum += node->val;  // 中
             if (cnt.contains(pre_sum - targetSum)) {
                 ans += cnt[pre_sum - targetSum];
             }
             ++cnt[pre_sum];  // 无论是否 contains 都要 ++ 防止漏掉键值对
 
-            dfs(node->left, pre_sum);   // 左
-            dfs(node->right, pre_sum);  // 右
+            dfs(node->left);   // 左
+            dfs(node->right);  // 右
 
-            --cnt[pre_sum];  // 恢复现场
+            --cnt[pre_sum];        // 恢复现场
+            pre_sum -= node->val;  // 恢复现场
         };
-        dfs(root, 0);  // 隐含 pre_sum[0] = 0
+        dfs(root);
         return ans;
     }
 };

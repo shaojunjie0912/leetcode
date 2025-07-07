3d98f8c6caf637dfb88997873ae4da2b6126aeb9
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-07-07 22:52:44 +0800

    update

--- 209.minimum-size-subarray-sum.cpp
+++ 209.minimum-size-subarray-sum.cpp
@@ -12,22 +12,23 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int min_len{INT_MAX};  // NOTE:
-        int curr_sum{0};
-        for (int l{0}, r{0}; r < nums.size(); ++r) {
-            curr_sum += nums[r];
-            while (curr_sum >= target) {  // 更新长度写在while里面那么条件就不用先减去l
-                // 先保存此时长度, 不然你就把当前长度丢了
-                min_len = min(min_len, r - l + 1);
-                // 再右移左边界
-                curr_sum -= nums[l++];
+        int ans{INT_MAX};
+        int n = nums.size();
+        int l = 0;
+        int sum = 0;
+        for (int r = 0; r < n; ++r) {
+            sum += nums[r];                    // 先加上右端点
+            while (sum - nums[l] >= target) {  // 尽量缩小长度
+                sum -= nums[l];
+                ++l;
+            }
+            if (sum >= target) {  // sum >= target 才更新, 可以跳过
+                ans = min(r - l + 1, ans);
             }
         }
-        return min_len == INT_MAX ? 0 : min_len;
+        return ans == INT_MAX ? 0 : ans;
     }
 };
 // @leet end
 
-int main() {
-    return 0;
-}
+int main() { return 0; }

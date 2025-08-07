a645feb8db91091124f4ab4b0f683d0520f10f8b
shaojunjie0912 <shaojunjie0912@outlook.com>
2025-08-07 20:26:04 +0800

    update

--- 215.kth-largest-element-in-an-array.cpp
+++ 215.kth-largest-element-in-an-array.cpp
@@ -9,10 +9,35 @@ using namespace std;
 class Solution {
 public:
 #if 1
+    // 快速选择(时空复杂度都是 O(n))
+    // 随机选择基准数字, 划分 big equal small
+    int findKthLargest(vector<int>& nums, int k) {
+        // 随机选择基准元素
+        int p = nums[rand() % nums.size()];
+
+        // 遍历 nums 与基准元素比较分 small(<p) equal(=p) big(>p) 子数组
+        vector<int> big, equal, small;
+        for (auto& x : nums) {
+            x > p ? big.push_back(x) : (x < p ? small.push_back(x) : equal.push_back(x));
+        }
+        // 如果 k 小于 big 数组大小, 则说明第 k 大的元素在 big 中
+        if (k <= big.size()) {
+            return findKthLargest(big, k);
+        }
+
+        // 如果 k 大于 big + equal 数组大小之和, 则说明第 k 大的元素在 small 中
+        if (k > big.size() + equal.size()) {
+            return findKthLargest(small, k - big.size() - equal.size());  // NOTE: 这里得改 k
+        }
+
+        // 第 k 大在 equal 中 (即就是 p)
+        return p;
+    }
+#else
     // 最小堆
     int findKthLargest(vector<int>& nums, int k) {
         // 维护大小为 k 的最小堆, 最终堆顶即第 k 大元素
-        priority_queue<int, vector<int>, greater<int>> min_heap;
+        priority_queue<int, vector<int>, greater<int> > min_heap;
         for (auto& x : nums) {
             min_heap.push(x);
             if (min_heap.size() > k) {
@@ -20,36 +45,6 @@ public:
             }
         }
         return min_heap.top();
-    }
-
-#else
-    // 快速选择
-    // 随机选择基准数字
-    // 划分 big equal small
-    // 递归 big small 最后返回 p
-    int findKthLargest(vector<int>& nums, int k) { return QuickSelect(nums, k); }
-
-    int QuickSelect(vector<int>& nums, int k) {
-        // 随机选择基准数字
-        int p = nums[rand() % nums.size()];
-
-        // 遍历 nums 划分 >, =, <
-        vector<int> big, equal, small;
-        for (auto& x : nums) {
-            x > p ? big.push_back(x) : (x < p ? small.push_back(x) : equal.push_back(x));
-        }
-        // 第 k 大在 big 中
-        if (k <= big.size()) {
-            return QuickSelect(big, k);
-        }
-
-        // 第 k 大在 small 中
-        if (k > big.size() + equal.size()) {
-            return QuickSelect(small, k - big.size() - equal.size());  // NOTE: 这里得改 k
-        }
-
-        // 第 k 大在 equal 中
-        return p;
     }
 #endif
 };
